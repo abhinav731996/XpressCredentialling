e36710659607795e26b76c165738e132e9ed0003
taif-k <taifk817@gmail.com>
2025-08-24 16:45:52 +0530

    test paths updated

--- src/domain/path/project_paths.py
+++ src/domain/path/project_paths.py
@@ -10,7 +10,14 @@ class PATHS:
     florida_med_board_url = "https://mqa-internet.doh.state.fl.us/MQASearchServices/HealthCareProviders"
 
     temp_output_dir = Path("temp_outputs")
-    license_for_cms = r"D:\Repositories\XpressCredentialling\src\database\npi_res.xlsx"
     email_sheet = r"D:\Repositories\XpressCredentialling\src\database\email_sheet.xlsx"
+
+    #test 
+    improperstate_email_ex_sheet = r"D:\Repositories\XpressCredentialling\src\database\test\test_email_sheet_improperstate.xlsx"
+    improperstate_npi_ex_sheet = r"D:\Repositories\XpressCredentialling\src\database\test\test_npi_improperstate.xlsx"
+    
+    normalstate_email_ex_sheet = r"D:\Repositories\XpressCredentialling\src\database\test\test_email_sheet.xlsx"
+    normalstate_npi_ex_sheet = r"D:\Repositories\XpressCredentialling\src\database\test\test_npi.xlsx"
+
 path_obj = PATHS()
 
