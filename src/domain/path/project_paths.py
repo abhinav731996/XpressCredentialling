90266edfad43e495c34c83e2a31f098cd404e9f6
taif-k <taifk817@gmail.com>
2025-08-26 14:07:12 +0530

    test path added

--- src/domain/path/project_paths.py
+++ src/domain/path/project_paths.py
@@ -3,6 +3,9 @@ from pathlib import Path
 class PATHS:
     error_details_file = r"D:\Repositories\XpressCredentialling\src\log\error_details.txt"
     
+    license_for_cms = r"D:\Repositories\XpressCredentialling\src\database\license_for_client_npi.xlsx"
+
+    request_errors_log = r"D:\Repositories\XpressCredentialling\src\log\req_err_log.txt"
     npi_url = "https://npiregistry.cms.hhs.gov/api/" 
     npi_excel_file = r"D:\Repositories\XpressCredentialling\src\database\npi_data.xlsx"    
 
@@ -12,6 +15,11 @@ class PATHS:
     temp_output_dir = Path("temp_outputs")
     email_sheet = r"D:\Repositories\XpressCredentialling\src\database\email_sheet.xlsx"
 
+    # Arizona
+    arizona_input_famprac = r"D:\Repositories\XpressCredentialling\src\database\arizona_famprac.xlsx"
+    arizona_npi_license = r"D:\Repositories\XpressCredentialling\src\database\arizona_npi_license.xlsx"
+    arizona_lookup_url = r"https://azbomv7prod.glsuite.us/GLSuiteWeb/Clients/AZBOM/public/WebVerificationSearch.aspx?q=azmd&t=20250816101430"
+    arizona_test_npi = r"D:\Repositories\XpressCredentialling\src\database\test\arizona_test\rename_test_npi.xlsx"
     #test 
     improperstate_email_ex_sheet = r"D:\Repositories\XpressCredentialling\src\database\test\test_email_sheet_improperstate.xlsx"
     improperstate_npi_ex_sheet = r"D:\Repositories\XpressCredentialling\src\database\test\test_npi_improperstate.xlsx"
