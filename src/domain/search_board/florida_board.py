a1b58874a32ef0dc40b34d475aff3c20b5f309e0
taif-k <taifk817@gmail.com>
2025-08-27 22:19:07 +0530

    grabbing education details also

--- src/domain/search_board/florida_board.py
+++ src/domain/search_board/florida_board.py
@@ -6,22 +6,11 @@ from selenium.webdriver.common.by import By
 from src.domain.file_io.io_file import ERRORIO
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from src.domain.helper.name_match import name_match_obj
 
 class Florida:
     def __init__(self):
         pass
-
-    def normalize_name(self,name):
-        name = name.lower()
-        name = re.sub(r'[^\w\s]', '', name)
-        return name.split()
-
-    def names_have_overlap(self,input_first, input_last, florida_name_raw):
-        input_parts = self.normalize_name(f"{input_first} {input_last}")
-        florida_parts = self.normalize_name(florida_name_raw)
-
-        common = set(input_parts) & set(florida_parts)
-        return len(common) > 0
 
     def enter_details(self, driver, wait, **all_info):
         try:
@@ -74,13 +63,16 @@ class Florida:
 
                     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.tab-content div.tab-pane.active")))
 
-
                     to_upper_divs = driver.find_elements(By.CSS_SELECTOR, "div.tab-pane.active div.toUpper")
                     if len(to_upper_divs) >= 5:
-                        name = to_upper_divs[0].text.strip()
-                        if not self.names_have_overlap(first_name, last_name, name):
+                        site_name = to_upper_divs[0].text.strip()
+                        is_matched = name_match_obj.is_name_match(first_name, last_name, site_name)
+                        if not is_matched:
                             continue
-                        primary_address = " ".join([d.text.strip() for d in to_upper_divs[1:5]])
+                        address_parts = []
+                        for div in to_upper_divs[1:5]:
+                            address_parts.append(div.text.strip())
+                        primary_address = " ".join(address_parts)    
                     else:
                         name = ""
                         primary_address = ""
@@ -95,17 +87,44 @@ class Florida:
 
                     email = email_p.find_element(By.TAG_NAME, "strong").text.strip() if email_p else ""
 
+                    edu_tab = wait.until(EC.element_to_be_clickable(
+                        (By.CSS_SELECTOR, "#content > div > div:nth-child(8) > ul > li:nth-child(2) > a")
+                    ))
+                    edu_tab.click()
+
+    
+                    wait.until(EC.presence_of_element_located((By.ID, "EducationAndTraining")))
+
+                    education_data = []
+                    indices = [3, 6, 9]
+
+                    for idx in indices:
+                        try:
+                            selector = f"#EducationAndTraining > table:nth-child({idx}) > tbody > tr > td:nth-child(1)"
+                            edu_td = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
+                            education_data.append(edu_td.text.strip())
+                        except:
+                            education_data.append("")
+
+                    education_data = (education_data + ["", "", ""])[:3]
+         
                     data = {
                         "npi": npi_number,
                         "license_number": [license_number_found],
                         "primary_address": [primary_address],
                         "name": [name],
-                        "email": [email]
+                        "email": [email],
+                        "institution_name": [education_data[0]],
+                        "university_school": [education_data[1]],
+                        "program_name": [education_data[2]]
                     }
                     result_df = pd.DataFrame(data)
+                    
                     return result_df
 
                 except Exception:
+                    err_obj = ERRORIO()
+                    err_obj.write_file(err)
                     continue
 
             return None 
