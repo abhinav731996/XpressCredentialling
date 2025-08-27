afc7abfb8383b7d5ff4b8056c9246f5909f367a9
taif-k <taifk817@gmail.com>
2025-08-27 22:22:22 +0530

    npi navigate on AZ & FL accordingly

--- src/domain/search_board/main_search/state_select.py
+++ src/domain/search_board/main_search/state_select.py
@@ -3,6 +3,7 @@ import os, sys, ast
 import time, random,re
 sys.path.append(os.getcwd())
 from src.domain.search_board.florida_board import fl_obj
+from src.domain.search_board.arizona_board import az_obj
 from src.domain.file_io.io_file import ERRORIO
 from src.domain.path.project_paths import path_obj
 from selenium import webdriver
@@ -11,14 +12,17 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from pathlib import Path
-from src.domain.search_board.google_api import GOOGLESEARCAPI 
+# import tempfile
+# import shutil
+
+# from src.domain.search_board.google_api import GOOGLESEARCAPI 
 
 class Med_info:
     def __init__(self):
         pass
 
     def clean_license(self, df, state):
-        if state in ["TX", "FL"] and "license_number" in df.columns:
+        if state in ["AZ", "FL"] and "license_number" in df.columns:
             df["license_number"] = df["license_number"].astype(str).str.replace("License Number:", "").str.strip()
         return df
 
@@ -29,9 +33,10 @@ class Med_info:
             output_dir = Path(path_obj.temp_output_dir)
             output_dir.mkdir(parents=True, exist_ok=True)
 
-            output_file = output_dir / f"{select_state}_chunk_{chunk_id}.xlsx"
+            output_file = output_dir / f"chunk_{chunk_id}.xlsx"
             df.to_excel(output_file, index=False)
             print(f"[{os.getpid()}] Wrote chunk to {output_file}")
+            # print(df)
         except Exception as err:
             err_obj = ERRORIO()
             err_obj.write_file(err)
@@ -55,17 +60,27 @@ class Med_info:
             err_obj.write_file(err)
 
 
-    def enter_info(self, npi_df: pd.DataFrame, state_code: str, chunk_id: int):
+    def enter_info(self, npi_df: pd.DataFrame,  chunk_id: int):
         try:
+            print(chunk_id)
             options = Options()
             prefs = {"profile.managed_default_content_settings.images": 2}
             options.add_experimental_option("prefs", prefs)
-            # options.add_argument("--headless=new")
-            options.add_argument("--start-maximized")
-            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-            wait = WebDriverWait(driver, 5)
+            options.add_argument("--headless=new")
+            # options.add_argument('-–start-maximized')
+            options.add_argument("--log-level=0")  # 0 = ALL, 3 = SEVERE
+            options.add_experimental_option("excludeSwitches", ["enable-logging"])
+            # options.add_argument("--no-sandbox")
+
+            driver_path = r"C:\Users\DELL\.wdm\drivers\chromedriver\win64\139.0.7258.154\chromedriver-win32\chromedriver.exe"
+            service = Service(driver_path)
+            driver = webdriver.Chrome(service=service, options=options)
+
+            # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+            wait = WebDriverWait(driver, 10)
 
             results = []
+            selected_state = {"FL","AZ"}
 
             for _, record in npi_df.iterrows():
                 npi_number = str(record["number"]).strip()
@@ -81,8 +96,8 @@ class Med_info:
                 else:
                     taxonomies = row_taxonomies
 
-                fl_primary_taxonomy = None
-                fl_any_taxonomy = None
+                state_primary_taxonomy = None
+                state_any_taxonomy = None
 
                 for taxo in taxonomies:
                     if not isinstance(taxo, dict):
@@ -91,25 +106,34 @@ class Med_info:
                     state = (taxo.get("state") or "").strip().upper()
                     is_primary = taxo.get("primary", False)
 
-                    if state == "FL" and is_primary:
-                        fl_primary_taxonomy = taxo
-                        break  
-                    elif state == "FL" and not fl_any_taxonomy:
-                        fl_any_taxonomy = taxo 
+                    
 
-                selected_taxonomy = fl_primary_taxonomy or fl_any_taxonomy
+                    if state in selected_state and is_primary:
+                        state_primary_taxonomy = taxo
+                        break  
+                    elif state in selected_state and not state_any_taxonomy:
+                        state_any_taxonomy = taxo 
+
+                selected_taxonomy = state_primary_taxonomy or state_any_taxonomy
 
                 if selected_taxonomy:
-                    license_number = (selected_taxonomy.get("license") or "").strip()
-                    taxonomy_state = "FL"
+                    license_number = (selected_taxonomy.get("license") or "").strip() 
+                    taxonomy_state = (selected_taxonomy.get("state") or "").strip().upper()
 
-                    if re.fullmatch(r"\d+", license_number):
+                    if re.fullmatch(r"\d+", license_number) and taxonomy_state == "FL":
                         license_variants = [f"ME{license_number}", f"OS{license_number}"]
-                    else:
-                        license_variants = self.normalize_license(license_number)
+
+                    elif taxonomy_state  == "AZ":
+                        match = re.fullmatch(r"(az)?(\d+)", license_number, re.IGNORECASE)
+                        if match:
+                            license_variants = [match.group(2)] 
+                        # license_variants = [license_number]    
+                        else:
+                            license_variants = self.normalize_license(license_number)
                 else:
+                    print(f"[{os.getpid()}] No results found for chunk {chunk_id}")
                     license_number = ""
-                    taxonomy_state = state_code
+                    taxonomy_state = ""
                     license_variants = []
 
                 all_info = {
@@ -129,16 +153,20 @@ class Med_info:
                         if state_df_result is not None and not state_df_result.empty:
                             results.append(state_df_result)
                         else:       
-                            print("Go through google search api")                     
-                            search_api_obj = GOOGLESEARCAPI()
-                            fallback_data = search_api_obj.get_details_searchapi(all_info["first_name"], all_info["last_name"],all_info["npi_number"])
+                            print("Try with search api")                     
+                            # search_api_obj = GOOGLESEARCAPI()
+                            # fallback_data = search_api_obj.get_details_searchapi(all_info["first_name"], all_info["last_name"],all_info["npi_number"])
 
-                            if fallback_data:
-                                fallback_df = pd.DataFrame(fallback_data)
-                                results.append(fallback_df)
+                            # if fallback_data:
+                            #     fallback_df = pd.DataFrame(fallback_data)
+                            #     results.append(fallback_df)
                                 
-                            else:
-                                print("Fallback also failed. No data found.")
+                            # else:
+                            #     print("Fallback also failed. No data found.")
+                    elif taxonomy_state == "AZ":
+                        state_df_result = az_obj.enter_details(driver, wait, **all_info)
+                        if state_df_result is not None and not state_df_result.empty:
+                            results.append(state_df_result)      
                     else:
                         print(f"Unsupported state: {taxonomy_state}")
                         continue
@@ -150,10 +178,10 @@ class Med_info:
 
             if results:
                 final_df = pd.concat(results, ignore_index=True)
-                self.write_file(state_code, final_df, chunk_id)
-
+                self.write_file(taxonomy_state, final_df, chunk_id)
             driver.quit()
 
         except Exception as err:
             err_obj = ERRORIO()
             err_obj.write_file(err)
+ 
