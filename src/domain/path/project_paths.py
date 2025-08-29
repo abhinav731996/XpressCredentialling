77f349bfb677402d855c2d4b726a684be3a73677
taif-k <taifk817@gmail.com>
2025-08-29 09:02:28 +0530

    descriptive variables used

--- src/domain/path/project_paths.py
+++ src/domain/path/project_paths.py
@@ -1,33 +1,23 @@
 from pathlib import Path
 
 class PATHS:
-    error_details_file = r"D:\Repositories\XpressCredentialling\src\log\error_details.txt"
-    
-    license_for_cms = r"D:\Repositories\XpressCredentialling\src\database\license_for_client_npi.xlsx"
-
-    request_errors_log = r"D:\Repositories\XpressCredentialling\src\log\req_err_log.txt"
+    # npi
     npi_url = "https://npiregistry.cms.hhs.gov/api/" 
-    npi_excel_file = r"D:\Repositories\XpressCredentialling\src\database\npi_data.xlsx"    
 
-    xpress_input = r"D:\Repositories\XpressCredentialling\src\database\XpressCred_DataSample.xlsx"
+    # FL
     florida_med_board_url = "https://mqa-internet.doh.state.fl.us/MQASearchServices/HealthCareProviders"
 
-    temp_output_dir = Path("temp_outputs")
-    email_sheet = r"D:\Repositories\XpressCredentialling\src\database\email_sheet.xlsx"
-
     # Arizona
-    arizona_input_famprac = r"D:\Repositories\XpressCredentialling\src\database\test\arizona_get_license.xlsx"
-    arizona_npi_license_two = r"D:\Repositories\XpressCredentialling\src\database\test\arizona_npi_license_two.xlsx"
+    client_az_sheet_npi = r"D:\Repositories\XpressCredentialling\src\database\client_az_sheet_npi.xlsx"
+    az_npi_details_api = r"D:\Repositories\XpressCredentialling\src\database\az_npi_details_api.xlsx"
     arizona_lookup_url = r"https://azbomv7prod.glsuite.us/GLSuiteWeb/Clients/AZBOM/public/WebVerificationSearch.aspx?q=azmd&t=20250816101430"
-    arizona_test_npi = r"D:\Repositories\XpressCredentialling\src\database\test\arizona_test\rename_test_npi.xlsx"
-    #test 
-    improperstate_email_ex_sheet = r"D:\Repositories\XpressCredentialling\src\database\test\test_email_sheet_improperstate.xlsx"
-    improperstate_npi_ex_sheet = r"D:\Repositories\XpressCredentialling\src\database\test\test_npi_improperstate.xlsx"
-    
-    normalstate_email_ex_sheet = r"D:\Repositories\XpressCredentialling\src\database\test\test_email_sheet.xlsx"
-    normalstate_npi_ex_sheet = r"D:\Repositories\XpressCredentialling\src\database\test\test_combined.xlsx"
 
-    website_sheet = r"D:\Repositories\XpressCredentialling\src\database\test\test_combined.xlsx"
-    website_npi_info = r"D:\Repositories\XpressCredentialling\src\database\raish_dinesh_npi_info.xlsx"
+    # Log
+    error_details_file = r"D:\Repositories\XpressCredentialling\src\log\error_details.txt"
+    request_errors_log = r"D:\Repositories\XpressCredentialling\src\log\req_err_log.txt"
+    
+    # DataBase
+    temp_output_dir = Path(r"D:\Repositories\XpressCredentialling\src\database\temp_outputs")
+
 path_obj = PATHS()
 
