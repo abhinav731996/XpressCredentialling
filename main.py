73bebc8780f1e647f1c9b37afeefc90219756d1e
taif-k <taifk817@gmail.com>
2025-10-25 17:53:54 +0530

    calling inside __name__

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from src.domain.scraper.run_parallel_scraper import SCRAPPER
-from src.domain.get_npi.fetch_npi import NPI_API
+if __name__ == "__main__":
+    import multiprocessing
+    multiprocessing.freeze_support() 
 
+    from src.domain.get_npi.fetch_npi import NPI_API
+    from src.domain.scraper.run_parallel_scraper import SCRAPPER
 
-NPI_API().api_fetch() # first, details for client npi are fetched
-scraper_obj = SCRAPPER()
-scraper_obj.create_instances()  # second, details are navigated to respective state board lookups
-scraper_obj.merge_outputs() 
+    NPI_API().api_fetch()
+
+    scraper_obj = SCRAPPER()
+    scraper_obj.create_instances()
+
+    scraper_obj.merge_outputs()
