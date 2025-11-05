6bb6aef6749b98332817fb3b2fa64377704cf651
taif-k <taifk817@gmail.com>
2025-11-05 08:36:50 +0530

    unique and set similarity cmnt added

--- src/domain/get_npi/fetch_npi.py
+++ src/domain/get_npi/fetch_npi.py
@@ -11,7 +11,7 @@ class NPI_API:
     def __init__(self):
         npi_checker = NPIMATCH(check_type='client')
         self.df = pd.read_excel(path_obj.all_states_surgery_npi_test)
-        self.npi_list = self.df["National Provider Identifier"].dropna().astype(str).unique().tolist()
+        self.npi_list = self.df["National Provider Identifier"].dropna().astype(str).unique().tolist() # unique() in pandas is similar to set() in python
         self.npi_to_process  = npi_checker.npi_present_already()
         
         before_count = len(self.npi_list)
